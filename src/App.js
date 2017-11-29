a51d965802b89df8df2baa70aca6e8c45638da42
Cristian Muscalu <c_muscalu@mgcorp.co>
2017-11-29 12:23:56 +0200

    created a BlackBox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  background: #333;
+`
+const ImageBox = styled.div`
+  width: 600px;
+  height: 400px;
+  background: url('https://cdn-images-1.medium.com/max/2000/1*8v-jC0gOxqP2oOaTY32w_A.jpeg');
+  background-size: cover;
+  background-position: center;
+`
+
+const BlackBox = styled.div`
+  height: 25%;
+  width: 100%;
+  background: #000;
+  transform: scaleX(0.25);
+  transform-origin: right center;
+`
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-
-      </div>
+      <Wrapper>
+        <ImageBox>
+          <BlackBox />
+        </ImageBox>
+      </Wrapper>
     );
   }
 }
